2336a0b17806d42663651bcfb81f542b1a37650b
Videlin Donchev <ya.5164646777752015@yandex.ru>
2016-10-06 17:13:42 +0300

    solve wih object instead of Map

--- JavaScriptBasics-31January2016/04.minka-the-js-godess.js
+++ JavaScriptBasics-31January2016/04.minka-the-js-godess.js
@@ -3,32 +3,32 @@ function helpMinka(data) {
         return Math.round((sum / count) * 100) / 100;
     }
 
-    let catalogue = new Map;
+    let catalogue = {};
     for (let line of data) {
         let [name, type, number, score, lines] = line.split(/\s+&\s+/g).filter(t => t != '');
         let taskNumber = 'Task ' + number;
-        if (!catalogue.has(taskNumber)) {
-            catalogue.set(taskNumber, {tasks: [], average: 0, lines: 0, score: 0});
+        if (!catalogue.hasOwnProperty(taskNumber)) {
+            catalogue[taskNumber] = {tasks: [], average: 0, lines: 0, score: 0};
         }
 
-        catalogue.get(taskNumber).tasks.push({name: name, type: type});
-        catalogue.get(taskNumber).lines += Number(lines);
-        catalogue.get(taskNumber).score += Number(score);
-        catalogue.get(taskNumber).average = calculateAverage(
-            catalogue.get(taskNumber).score,
-            catalogue.get(taskNumber).tasks.length);
+        catalogue[taskNumber].tasks.push({name: name, type: type});
+        catalogue[taskNumber].lines += Number(lines);
+        catalogue[taskNumber].score += Number(score);
+        catalogue[taskNumber].average = calculateAverage(
+            catalogue[taskNumber].score,
+            catalogue[taskNumber].tasks.length);
     }
 
-    for (let key of catalogue.keys()) {
-        delete catalogue.get(key).score;
+    for (let key of Object.keys(catalogue)) {
+        delete catalogue[key].score;
     }
 
     function sortCatalogue(itemA, itemB, catalogue) {
-        let itemAavg = catalogue.get(itemA).average;
-        let itemBavg = catalogue.get(itemB).average;
+        let itemAavg = catalogue[itemA].average;
+        let itemBavg = catalogue[itemB].average;
         if (itemAavg === itemBavg) {
-            let itemAlines = catalogue.get(itemA).lines;
-            let itemBlines = catalogue.get(itemB).lines;
+            let itemAlines = catalogue[itemA].lines;
+            let itemBlines = catalogue[itemB].lines;
 
             return itemAlines - itemBlines;
         }
@@ -36,14 +36,14 @@ function helpMinka(data) {
         return itemBavg - itemAavg;
     }
 
-    let output = {};
-    let sortedNumbers = [...catalogue.keys()].sort((a, b) => sortCatalogue(a, b, catalogue));
+    let sortedNumbers = [...Object.keys(catalogue)].sort((a, b) => sortCatalogue(a, b, catalogue));
+    let res = {};
     for (let num of sortedNumbers) {
-        output[num] = catalogue.get(num);
-        output[num].tasks = catalogue.get(num).tasks.sort((a, b) => a.name.localeCompare(b.name));
+        catalogue[num].tasks = catalogue[num].tasks.sort((a, b) => a.name.localeCompare(b.name));
+        res[num] = catalogue[num];
     }
 
-    console.log(JSON.stringify(output));
+    console.log(JSON.stringify(res));
 }
 
 helpMinka(["Array Matcher & strings & 4 & 100 & 38", "Magic Wand & draw & 3 & 100 & 15", "Dream Item & loops & 2 & 88 & 80", "Knight Path & bits & 5 & 100 & 65", "Basket Battle & conditionals & 2 & 100 & 120", "Torrent Pirate & calculations & 1 & 100 & 20", "Encrypted Matrix & nested loops & 4 & 90 & 52", "Game of bits & bits & 5 &  100 & 18", "Fit box in box & conditionals & 1 & 100 & 95", "Disk & draw & 3 & 90 & 15", "Poker Straight & nested loops & 4 & 40 & 57", "Friend Bits & bits & 5 & 100 & 81"]);
